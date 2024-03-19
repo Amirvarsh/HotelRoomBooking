b9bcc6907535717801e69c02f964ea0c0e855087
AMIRTHA VARSHAN K <117556964+Amirvarsh@users.noreply.github.com>
2024-03-20 00:40:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const bookingsRoute = require("./routes/bookingsRoute");
 
 app.use(
   cors({
-    origin: ["https://hotel-room-booking-1mzg.vercel.app/"],
+    origin: ["https://hotel-room-booking-1mzg.vercel.app"],
     method: ["POST", "GET"],
     credentials: true,
   })
